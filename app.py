2ae1a1690981b8319c39e6b6425d5ddbff292963
Gold Edem Hogan <edge37@outlookcom>
2024-03-02 05:11:45 -1200

    .

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from gradio_client import Client
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def convert_string_to_array():
-    return Client("edge37/embed", hf_token=os.environ.get("HF")).predict(request.get_data(as_text=true), api_name="/predict")
+    return Client("edge37/embed", hf_token=os.environ.get("HF")).predict(request.get_data(as_text=True), api_name="/predict")
 if __name__ == '__main__':
     app.run(debug=True)
