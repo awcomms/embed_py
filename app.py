72a019309dfd0e6f53234a224194075272290d43
Gold Edem Hogan <edge37@outlookcom>
2024-03-08 15:45:10 -1200

    .

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
-import os
-from gradio_client import Client
+from sentence_transformers import SentenceTransformer
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def convert_string_to_array():
-    return Client("edge37/embed", hf_token=os.environ.get("HF")).predict(request.get_data(as_text=True), api_name="/predict")
+    return SentenceTransformer("jinaai/jina-embedding-s-en-v1").encode(request.get_data(as_text=True))[0]
 if __name__ == '__main__':
-    app.run(debug=True)
+    import bjoern
+    bjoern.run(app, '0.0.0.0', 10000)
